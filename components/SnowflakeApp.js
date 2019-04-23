fbad1d5b58a42203c63f3f713a9f815138f15106
Alexis Smirnov <alexis@smirnov.ca>
2019-04-23 14:55:39 -0400

    copyright/logo

--- components/SnowflakeApp.js
+++ components/SnowflakeApp.js
@@ -200,10 +200,8 @@ class SnowflakeApp extends React.Component<Props, SnowflakeAppState> {
             handleTrackMilestoneChangeFn={(track, milestone) => this.handleTrackMilestoneChange(track, milestone)} />
         <div style={{display: 'flex', paddingBottom: '20px'}}>
           <div style={{flex: 1}}>
-            Made with ❤️ by <a href="https://medium.engineering" target="_blank">Medium Eng</a>.
-            Learn about the <a href="https://medium.com/s/engineering-growth-framework" target="_blank">growth framework</a>.
-            Get the <a href="https://github.com/Medium/snowflake" target="_blank">source code</a>.
-            Read the <a href="https://medium.com/p/85e078bc15b7" target="_blank">terms of service</a>.
+            Made with ❤️ by <a href="https://dialogue.co" target="_blank">Dialogue Eng</a>.
+            Based on Medium's <a href="https://medium.com/s/engineering-growth-framework" target="_blank">Engineering Growth framework</a>.
           </div>
         </div>
       </main>
--- components/Wordmark.js
+++ components/Wordmark.js
@@ -1,2 +1,2 @@
 // @flow
-export default () => <img src="https://383sqt41skp3b9dim452n19e-wpengine.netdna-ssl.com/wp-content/themes/dialogue/library/img/logo-blue@2x.png"></img>
+export default () => <img src="https://www.dialogue.co/hubfs/Dialogue_March2019/Images/logo-blue@2x.png"></img>
